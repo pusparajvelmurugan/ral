181cb202165620d9e8b00370e96dfd2753d62d88
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-02-02 16:11:16 +0530

    Update cak.c

--- cak.c
+++ cak.c
@@ -1,38 +1,50 @@
-#include <stdio.h> 
-void main()
+
+#include<stdlib.h>
+#include<stdio.h> 
+void merge(int arr[], int l, int m, int r)
 {
-    int array[20];
-    int i, j, num, temp;
- 
-    printf("Enter the value of num \n");
-    scanf("%d", &num);
-    printf("Enter the elements one by one \n");
-    for (i = 0; i < num; i++)
-    {
-        scanf("%d", &array[i]);
-    }
-    printf("Input array is \n");
-    for (i = 0;i<num<j)
-    {
-        printf("%d\n", array[i]);
-    }
-    for (i = 0;i<numj)
-    {
-        for (j = 0; j < (num - i - 1))
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-    printf("Sorted array is...\n");
-    for (i = 0;i<num<j)
-    {
-        printf("%d\n", array[i]);
-    }
+int i, j, k;
+int n1 = m - l + 1;
+int n2 =  r - m;
+int L[n1], R[n2];
+for (i = 0; i < n1; i++)
+L[i] = arr[l + i];
+for (j = 0; j < n2; j++)
+R[j] = arr[m + 1+ j];
+i = 0; 
+j = 0;
+k = l; 
+while (i < n1 && j < n2)
+{
+if (L[i] <= R[j])
+{
+arr[k] = L[i];
+i++;
 }
-getch();
+ else
+{
+arr[k] = R[j];
+j++;}
+ k++}
+ while (i < n1)
+ {
+ arr[k] = L[i];
+ k++;
+}
+while (j < n2)
+{
+ arr[k] = R[j];
+ j++;
+k++;
+}
+}
+void mergeSort(int arr[], int l, int r)
+{
+if (l < r)
+{
+int m = l+(r-l)/2;
+ mergeSort(arr, l, m);
+ mergeSort(arr, m+1, r); 
+merge(arr, l, m, r);
+    }
 }
