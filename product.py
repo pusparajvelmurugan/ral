8536f7b2dfe3e860bf1c29941a5fead937fc7374
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-02-23 15:57:25 +0530

    Create product.py

---
+++ product.py
@@ -0,0 +1,8 @@
+num1= int(input("Enter first number: "))
+num2 = int(input("enter second number:"))
+num3 = num1 * num2
+mod = num3 % 2
+if mod > 0:
+       print("This is an odd number.")
+else:
+       print("This is an even number.")
