a342d35f53c66ca9945defb9da76e0a4a16586c6
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-03-02 15:51:32 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter the number"))
+num2 = int(input("enter the number"))
+num = num1 + num2
+if (num % 2) == 0:
+    print("{0} is Even".format(num))
+else:
+    print("{0} is Odd".format(num))
