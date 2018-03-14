286a81f48680597bcaa0a4e7c0c7f6d3c18f7d41
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-03-14 16:36:07 +0530

    Create 95.py

---
+++ 95.py
@@ -0,0 +1,5 @@
+amount=int(input("Enter Amount:"))
+intrest=int(input("Enter interest:"))
+period=int(input("Enter Time(Month):"))
+totamt=amount*(intrest/100)*period
+print("Total amount is:",round(totamt))
