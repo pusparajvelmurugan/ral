424da022e31f5d5379f1192572985c6177d626be
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-03-14 16:52:57 +0530

    Update 94.py

--- 94.py
+++ 94.py
@@ -1,8 +1,8 @@
-n=int(input("Enter N value:"))
+num=(input("Enter N value:"))
 k=int(input("Enter K value:"))
 print("Enter the array values:")
 a=[]
-for x in range(0,n):
+for x in range(0,num):
 	t=int(input())
 	a.append(t)
 print(a[k-1])
