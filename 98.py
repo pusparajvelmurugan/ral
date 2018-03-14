db5862ae41a5f4d749b0354598a07b52df2b5e3f
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-03-14 16:12:54 +0530

    Update 98.py

--- 98.py
+++ 98.py
@@ -2,10 +2,10 @@ n=int(input("Enter n number:"))
 print("Enter ascending order:")
 a=[]
 for x in range(n):
-	k=int(input())
-	a.append(k)
-	for x in range(n-1):
-	  for y in range(x,n):
-		if(a[x]>a[y]):
-			ans=x+1
+k=int(input())
+a.append(k)
+for x in range(n-1):
+for y in range(x,n):
+if(a[x]>a[y]
+ans=x+1
 print(ans)
